3d0e6664aab7394c722f068f0f56ec6afa0ee87a
azizfedhil <azizfedhil@hotmail.com>
2026-05-29 17:55:16 +0100

    reset

--- shared/social-deduction/plugins/impostor.js
+++
@@ -1,6 +0,0 @@
-export const IMPOSTOR_PLUGIN = {
-    mode: 'impostor',
-    bodyClass: 'game-impostor',
-    supportsLangPills: true,
-    supportsImpostorSettings: true,
-};
--- shared/social-deduction/plugins/thief.js
+++
@@ -1,8 +0,0 @@
-export const THIEF_PLUGIN = {
-    mode: 'thief',
-    bodyClass: 'game-thief',
-    supportsLangPills: false,
-    supportsImpostorSettings: false,
-    voteButtonText: '⚖️ يا حاكم، احكم',
-    whoVoteText: 'يا حاكم، شكون السارق؟',
-};
